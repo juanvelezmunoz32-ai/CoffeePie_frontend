22bbebded1dc0ee272b91b8bd6f60555f71f3375
Juan Daniel Vélez Muñoz <273273118+juanvelezmunoz32-ai@users.noreply.github.com>
2026-08-31 15:07:14 -0500

    commit34367
    commit34367

--- start_coffeepie.sh
+++ start_coffeepie.sh
@@ -80,7 +80,9 @@ docker run \
     -e QML_FULLSCREEN=1 \
     -e QT_SCALE_FACTOR=1 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /sbin/shutdown:/sbin/shutdown \
+    --pid=host \
+    --privileged \
     $DOCKER_IMAGE
 
 echo "============================================"
